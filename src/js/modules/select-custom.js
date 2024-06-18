db0068eb1c7d0cb3b947eddfaffd1aca96160125
wolf2234 <viktoral9599@gmail.com>
2024-06-18 16:26:16 +0300

    this commit adds the addValueToSelect function.

--- src/js/modules/select-custom.js
+++ src/js/modules/select-custom.js
@@ -1,45 +1,56 @@
 export function createCustomSelect(selectClass) {
     let selectBlock = document.querySelector(selectClass);
 
-    let selectValue = document.createElement("div");
-    selectValue.className = "select-custom__value";
-    selectValue.innerHTML = selectBlock.value;
-
-    let selectItems = document.createElement("div");
-    selectItems.className = "select-custom__items";
-
-    for (let option of selectBlock.options) {
-        selectItems.innerHTML += `<div class="select-custom__item">${option.value}</div>`;
-    }
+    const selectClassCustom = "select-custom";
+    const selectClassCustomValue = "select-custom__value";
+    const selectClassCustomItems = "select-custom__items";
+    const selectClassCustomItem = "select-custom__item";
 
     let selectCustom = document.createElement("div");
-    selectCustom.className = "select-custom";
+    let selectItems = document.createElement("div");
+
+    selectCustom.className = selectClassCustom;
+    selectItems.className = selectClassCustomItems;
+
     selectCustom.innerHTML = `
             <div class="select-custom__body">
                 <div class="select-custom__current">
+                    <div class="select-custom__value"></div>
                     <span class="select-custom__icon"></span>
                 </div>
             </div>`;
 
-    let current = selectCustom.querySelector(".select-custom__current");
-    current.insertAdjacentElement("afterbegin", selectValue);
-    selectCustom.insertAdjacentElement("beforeend", selectItems);
+    for (let option of selectBlock.options) {
+        selectItems.innerHTML += `<div class="${selectClassCustomItem}">${option.value}</div>`;
+    }
 
+    let selectValue = selectCustom.querySelector(`.${selectClassCustomValue}`);
+    selectValue.innerText = selectBlock.value;
+
+    selectCustom.insertAdjacentElement("beforeend", selectItems);
     selectBlock.insertAdjacentElement("afterend", selectCustom);
     selectBlock.style.display = "none";
-
-    const selectsItems = selectItems.querySelectorAll(".select-custom__item");
 
     selectCustom.addEventListener("click", function (option) {
         selectCustom.classList.toggle("is-active");
     });
 
+    addValueToSelect(
+        selectCustom,
+        selectValue,
+        selectItems,
+        selectClassCustomItem
+    );
+}
+
+export function addValueToSelect(select, currentValue, items, itemClass) {
+    const selectsItems = items.querySelectorAll(`.${itemClass}`);
     selectsItems.forEach(function (selectItem) {
-        selectItem.addEventListener("click", function (selectOption) {
+        selectItem.addEventListener("click", function (item) {
             let text = selectItem.innerText;
             let currentText = selectItem
-                .closest(`.select-custom`)
-                .querySelector(`.select-custom__value`);
+                .closest(`.${select.classList[0]}`)
+                .querySelector(`.${currentValue.classList[0]}`);
             currentText.innerText = text;
         });
     });
