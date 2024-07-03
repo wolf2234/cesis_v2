43d8ac9116ca7b65b89eeff5ebbebc1af63bf5c2
wolf2234 <viktoral9599@gmail.com>
2024-07-03 15:53:27 +0300

    this commit adds comments for functions.js.

--- src/js/base/video.js
+++ src/js/base/video.js
@@ -1,8 +1,14 @@
+// Get all elements with class video__item.
 const videos = document.querySelectorAll(".video__item");
+
+// Get all elements with class video__button.
 const videoButtons = document.querySelectorAll(".video__button");
 
+// Go through a loop for each of buttons.
 videoButtons.forEach(function (button) {
+    // Listen user's click.
     button.addEventListener("click", function (element) {
+        // Go through a loop for each of video elements.
         videos.forEach(function (video) {
             video.addEventListener("pause", function (el) {
                 if (
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,8 +1,17 @@
+/**
+ * Show block, when user clicks on link.
+ * @param {number} defaultValue
+ */
 export function showBlock(defaultValue = 0) {
+    // Get all div elements with data-block-parent attribute.
     let parentElements = document.querySelectorAll("[data-block-parent]");
 
+    // Go through a loop for each of element.
     parentElements.forEach(function (parentElement) {
+        // Get all links with data-block-link attribute.
         const childlinks = parentElement.querySelectorAll("[data-block-link]");
+
+        // Get all items with data-block-item attribute.
         const childblocks = parentElement.querySelectorAll("[data-block-item]");
 
         if (defaultValue) {
@@ -10,6 +19,7 @@ export function showBlock(defaultValue = 0) {
             addActiveByDefault(childblocks, defaultValue);
         }
 
+        // Go through a loop for each of links.
         childlinks.forEach(function (childlink) {
             childlink.addEventListener("click", function (element) {
                 removeActives(childlinks);
@@ -17,6 +27,8 @@ export function showBlock(defaultValue = 0) {
 
                 let attrElement = childlink.dataset.blockLink;
                 childlink.classList.add("active");
+
+                // Go through a loop for each of items.
                 childblocks.forEach(function (childblock) {
                     let attrBlock = childblock.dataset.blockItem;
                     if (attrBlock === attrElement) {
@@ -28,6 +40,12 @@ export function showBlock(defaultValue = 0) {
     });
 }
 
+/**
+ * Add active to the element.
+ * It is for to select default element.
+ * @param {object} elements
+ * @param {number} defaultValue
+ */
 function addActiveByDefault(elements, defaultValue) {
     elements.forEach(function (element) {
         let attrElement;
@@ -42,15 +60,25 @@ function addActiveByDefault(elements, defaultValue) {
     });
 }
 
+/**
+ * Remove active class from elements.
+ * @param {object} elements
+ */
 function removeActives(elements) {
     elements.forEach(function (element) {
         element.classList.remove("active");
     });
 }
 
+/**
+ * Add hover effect for related elements.
+ */
 export function hoverElement() {
+    // Get all elements with data-hover-block attribute.
     const blocks = document.querySelectorAll("[data-hover-block]");
+    // Go through a loop for each of elements.
     blocks.forEach(function (block) {
+        // Get all elements with data-hover-attr attribute.
         const hoverTags = block.querySelectorAll("[data-hover-attr]");
         hoverTags.forEach(function (hoverTag) {
             hover(hoverTags, hoverTag);
@@ -58,6 +86,10 @@ export function hoverElement() {
     });
 }
 
+/**
+ * Add active class, when the user mouseover on element.
+ * If the user mouseout on element, active class removes.
+ */
 function hover(tags, currentTag) {
     let attrTag = currentTag.dataset.hoverAttr;
     tags.forEach(function (tag) {
